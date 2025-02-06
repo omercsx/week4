ce6b0505c6cf57edc0e55946b6625e4e79b885a1
omercsx <omercagris@gmail.com>
2025-02-06 09:38:50 -0500

    Add CRUD routes for movies in moviesRoutes.js

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const { insertMovies } = require("../controllers/MovieController");
+const {
+  insertMovies,
+  getAllMovies,
+  getMovieById,
+  updateMovie,
+  deleteMovie,
+  createMovie,
+} = require("../controllers/MovieController");
 
 router.post("/import", insertMovies);
+
+// route to get all movies
+router.get("/", getAllMovies);
+
+// route to get a movie by id
+router.get("/:id", getMovieById);
+
+// route to update a movie
+router.put("/update/:id", updateMovie);
+
+// route to delete a movie
+router.delete("/delete/:id", deleteMovie);
+
+// route to create a movie
+router.post("/create", createMovie);
 
 module.exports = router;
